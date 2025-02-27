8c643c6a164b39762aa13effe1c5fdbd9998cec4
SebastianLoaiza10 <sebastian2003002@gmail.com>
2025-02-26 20:15:02 -0500

    Add the figma style login in page by updating loginForm.jsx & creating LoginPage.css

--- va_frontend/src/pages/loginForm.jsx
+++ va_frontend/src/pages/loginForm.jsx
@@ -1,5 +1,25 @@
+import './LoginPage.css';
+
 function LoginForm(){
-    return <h1>Login Form</h1>;
+    return (
+        <div className="login-container">
+            {/* Left Panel - Sign In */}
+            <div className="left-panel">
+            <h1>Sign In</h1>
+            <label>Username</label>
+            <input type="text" placeholder="Username" className="input-box" />
+            <label>Password</label>
+            <input type="password" placeholder="Password" className="input-box" />
+            <button className="sign-in-button">Sign in</button>
+            </div>
+
+            {/* Right Panel - Sign Up */}
+            <div className="right-panel">
+            <p>Don’t have an account?</p>
+            <button className="sign-up-button">Sign up</button>
+            </div>
+        </div>
+    );
 }
 
 export default LoginForm;
