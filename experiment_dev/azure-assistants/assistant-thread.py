4f446a0d768aa3a93b698882aad012f3cbaabdc5
L42ARO <89555610+L42ARO@users.noreply.github.com>
2025-03-13 09:07:15 -0400

    assitant api alternative

--- experiment_dev/azure-assistants/assistant-thread.py
+++ experiment_dev/azure-assistants/assistant-thread.py
@@ -1,4 +1,5 @@
 import os  
+import time
 from openai import AzureOpenAI  
 from dotenv import load_dotenv
 
@@ -29,7 +30,7 @@ assistant_id = "asst_QAA9ERdqNke90WjtS9iN0PI7"
 question = "Based on the questions on Homework 1, which lectures should I study?"
 
 thread = client.beta.threads.create(
-  messages=[ { "role": "user", "content": question} ],
+#   messages=[ { "role": "user", "content": question} ],
   tool_resources={
     "file_search": {
       "vector_store_ids": [vs_id]
@@ -37,43 +38,40 @@ thread = client.beta.threads.create(
   }
 )
 
-from typing_extensions import override
-from openai import AssistantEventHandler
- 
-class EventHandler(AssistantEventHandler):
-    @override
-    def on_text_created(self, text) -> None:
-        print(f"\nassistant > ", end="", flush=True)
+# Create a thread
+# thread = client.beta.threads.create()
 
-    @override
-    def on_tool_call_created(self, tool_call):
-        print(f"\nassistant > {tool_call.type}\n", flush=True)
-
-    @override
-    def on_message_done(self, message) -> None:
-        # print a citation to the file searched
-        message_content = message.content[0].text
-        annotations = message_content.annotations
-        citations = []
-        for index, annotation in enumerate(annotations):
-            message_content.value = message_content.value.replace(
-                annotation.text, f"[{index}]"
-            )
-            if file_citation := getattr(annotation, "file_citation", None):
-                cited_file = client.files.retrieve(file_citation.file_id)
-                citations.append(f"[{index}] {cited_file.filename}")
-
-        print(message_content.value)
-        print("\n".join(citations))
+# Add a user question to the thread
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content=question # Replace this with your prompt
+)
 
 
-# Then, we use the stream SDK helper
-# with the EventHandler class to create the Run
-# and stream the response.
 
-with client.beta.threads.runs.stream(
+# Run the thread
+run = client.beta.threads.runs.create(
+  thread_id=thread.id,
+  assistant_id=assistant_id
+)
+
+# Looping until the run completes or fails
+while run.status in ['queued', 'in_progress', 'cancelling']:
+  time.sleep(1)
+  run = client.beta.threads.runs.retrieve(
     thread_id=thread.id,
-    assistant_id=assistant_id,
-    event_handler=EventHandler(),
-) as stream:
-    stream.until_done()
+    run_id=run.id
+  )
+
+if run.status == 'completed':
+  messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+  )
+  print(messages)
+elif run.status == 'requires_action':
+  # the assistant requires calling some functions
+  # and submit the tool outputs back to the run
+  pass
+else:
+  print(run.status)
